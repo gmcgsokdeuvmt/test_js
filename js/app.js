275064cb9a979cbf6cd8f8e92b22c097c3977402
ran <mochimochi.morst@gmail.com>
2019-04-07 23:21:38 +0900

    update

--- js/app.js
+++ js/app.js
@@ -304,6 +304,9 @@ $(() => {
         alert('2');                
         let track2 = new SoundTrack(audioCtx);
         track2.setWhiteNoise();
+        alert('5');                
         container.append(track1.dom,track2.dom);
+        alert('6');
+        console.log(container.html());    
     }
 });
